844b851ff62d2d419e9328c55e2ab26191b7cbab
Davide Casale <davi0kprogramsthings@gmail.com>
2025-06-11 21:18:15 +0200

    Implement @wdk/wallet.

---
+++ src/wallet-account.d.ts
@@ -0,0 +1,177 @@
+/**
+ * @typedef {Object} KeyPair
+ * @property {Uint8Array} publicKey - The public key.
+ * @property {Uint8Array} privateKey - The private key.
+ */
+/**
+ * @typedef {Object} Transaction
+ * @property {string} to - The transaction's recipient.
+ * @property {number} value - The amount of native tokens to send to the recipient (in base unit).
+ */
+/**
+ * @typedef {Object} TransactionResult
+ * @property {string} hash - The transaction's hash.
+ * @property {number} fee - The gas cost.
+ */
+/**
+ * @typedef {Object} TransferOptions
+ * @property {string} token - The address of the token to transfer.
+ * @property {string} recipient - The address of the recipient.
+ * @property {number} amount - The amount of tokens to transfer to the recipient (in base units).
+ */
+/**
+ * @typedef {Object} TransferResult
+ * @property {string} hash - The hash of the transfer operation.
+ * @property {number} fee - The gas cost.
+ */
+export class NotImplementedError extends Error {
+    /**
+     * Create a new not implemented error.
+     * 
+     * @param {string} methodName - The method's name.
+     */
+    constructor(methodName: string);
+}
+/** @interface */
+export default interface IWalletAccount {
+    /**
+     * The derivation path's index of this account.
+     *
+     * @type {number}
+     */
+    get index(): number;
+    /**
+     * The derivation path of this account (see [BIP-44](https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki)).
+     *
+     * @type {string}
+     */
+    get path(): string;
+    /**
+     * The account's key pair.
+     *
+     * @type {KeyPair}
+     */
+    get keyPair(): KeyPair;
+    /**
+     * Returns the account's address.
+     *
+     * @returns {Promise<string>} The account's address.
+     */
+    getAddress(): Promise<string>;
+    /**
+     * Signs a message.
+     *
+     * @param {string} message - The message to sign.
+     * @returns {Promise<string>} The message's signature.
+     */
+    sign(message: string): Promise<string>;
+    /**
+     * Verifies a message's signature.
+     *
+     * @param {string} message - The original message.
+     * @param {string} signature - The signature to verify.
+     * @returns {Promise<boolean>} True if the signature is valid.
+     */
+    verify(message: string, signature: string): Promise<boolean>;
+    /**
+     * Returns the account's native token balance.
+     *
+     * @returns {Promise<number>} The native token balance.
+     */
+    getBalance(): Promise<number>;
+    /**
+     * Returns the account balance for a specific token.
+     *
+     * @param {string} tokenAddress - The smart contract address of the token.
+     * @returns {Promise<number>} The token balance.
+     */
+    getTokenBalance(tokenAddress: string): Promise<number>;
+    /**
+     * Sends a transaction.
+     *
+     * @param {Transaction} tx - The transaction.
+     * @returns {Promise<TransactionResult>} The transaction's result.
+     */
+    sendTransaction(tx: Transaction): Promise<TransactionResult>;
+    /**
+     * Quotes the costs of a send transaction operation.
+     *
+     * @see {sendTransaction}
+     * @param {Transaction} tx - The transaction.
+     * @returns {Promise<Omit<TransactionResult, 'hash'>>} The transaction's quotes.
+     */
+    quoteSendTransaction(tx: Transaction): Promise<Omit<TransactionResult, "hash">>;
+    /**
+     * Transfers a token to another address.
+     *
+     * @param {TransferOptions} options - The transfer's options.
+     * @returns {Promise<TransferResult>} The transfer's result.
+     */
+    transfer(options: TransferOptions): Promise<TransferResult>;
+    /**
+     * Quotes the costs of a transfer operation.
+     *
+     * @see {transfer}
+     * @param {TransferOptions} options - The transfer's options.
+     * @returns {Promise<Omit<TransferResult, 'hash'>>} The transfer's quotes.
+     */
+    quoteTransfer(options: TransferOptions): Promise<Omit<TransferResult, "hash">>;
+    /**
+     * Disposes the wallet account, erasing the private key from the memory.
+     */
+    dispose(): void;
+}
+export type KeyPair = {
+    /**
+     * - The public key.
+     */
+    publicKey: Uint8Array;
+    /**
+     * - The private key.
+     */
+    privateKey: Uint8Array;
+};
+export type Transaction = {
+    /**
+     * - The transaction's recipient.
+     */
+    to: string;
+    /**
+     * - The amount of native tokens to send to the recipient (in base unit).
+     */
+    value: number;
+};
+export type TransactionResult = {
+    /**
+     * - The transaction's hash.
+     */
+    hash: string;
+    /**
+     * - The gas cost.
+     */
+    fee: number;
+};
+export type TransferOptions = {
+    /**
+     * - The address of the token to transfer.
+     */
+    token: string;
+    /**
+     * - The address of the recipient.
+     */
+    recipient: string;
+    /**
+     * - The amount of tokens to transfer to the recipient (in base units).
+     */
+    amount: number;
+};
+export type TransferResult = {
+    /**
+     * - The hash of the transfer operation.
+     */
+    hash: string;
+    /**
+     * - The gas cost.
+     */
+    fee: number;
+};
---
+++ src/wallet-account.js
@@ -0,0 +1,186 @@
+// Copyright 2024 Tether Operations Limited
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+'use strict'
+
+/**
+ * @typedef {Object} KeyPair
+ * @property {Uint8Array} publicKey - The public key.
+ * @property {Uint8Array} privateKey - The private key.
+ */
+
+/**
+ * @typedef {Object} Transaction
+ * @property {string} to - The transaction's recipient.
+ * @property {number} value - The amount of native tokens to send to the recipient (in base unit).
+ */
+
+/**
+ * @typedef {Object} TransactionResult
+ * @property {string} hash - The transaction's hash.
+ * @property {number} fee - The gas cost.
+ */
+
+/**
+ * @typedef {Object} TransferOptions
+ * @property {string} token - The address of the token to transfer.
+ * @property {string} recipient - The address of the recipient.
+ * @property {number} amount - The amount of tokens to transfer to the recipient (in base units).
+ */
+
+/**
+ * @typedef {Object} TransferResult
+ * @property {string} hash - The hash of the transfer operation.
+ * @property {number} fee - The gas cost.
+ */
+
+export class NotImplementedError extends Error {
+  /**
+   * Create a new not implemented error.
+   * 
+   * @param {string} methodName - The method's name.
+   */
+  constructor(methodName) {
+    super(`Method '${methodName}' must be implemented.`)
+
+    this.name = 'NotImplementedError'
+  }
+}
+
+/** @interface */
+export default class IWalletAccount {
+  /**
+   * The derivation path's index of this account.
+   *
+   * @type {number}
+   */
+  get index() {
+    throw new NotImplementedError('index')
+  }
+
+  /**
+   * The derivation path of this account (see [BIP-44](https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki)).
+   *
+   * @type {string}
+   */
+  get path() {
+    throw new NotImplementedError('path')
+  }
+
+  /**
+   * The account's key pair.
+   *
+   * @type {KeyPair}
+   */
+  get keyPair() {
+    throw new NotImplementedError('keyPair')
+  }
+
+  /**
+   * Returns the account's address.
+   *
+   * @returns {Promise<string>} The account's address.
+   */
+  async getAddress() {
+    throw new NotImplementedError('getAddress()')
+  }
+
+  /**
+   * Signs a message.
+   *
+   * @param {string} message - The message to sign.
+   * @returns {Promise<string>} The message's signature.
+   */
+  async sign(message) {
+    throw new NotImplementedError('sign(message)')
+  }
+
+  /**
+   * Verifies a message's signature.
+   *
+   * @param {string} message - The original message.
+   * @param {string} signature - The signature to verify.
+   * @returns {Promise<boolean>} True if the signature is valid.
+   */
+  async verify(message, signature) {
+    throw new NotImplementedError('verify(message, signature)')
+  }
+
+  /**
+   * Returns the account's native token balance.
+   *
+   * @returns {Promise<number>} The native token balance.
+   */
+  async getBalance() {
+    throw new NotImplementedError('getBalance()')
+  }
+
+  /**
+   * Returns the account balance for a specific token.
+   *
+   * @param {string} tokenAddress - The smart contract address of the token.
+   * @returns {Promise<number>} The token balance.
+   */
+  async getTokenBalance(tokenAddress) {
+    throw new NotImplementedError('getTokenBalance(tokenAddress)')
+  }
+
+  /**
+   * Sends a transaction.
+   *
+   * @param {Transaction} tx - The transaction.
+   * @returns {Promise<TransactionResult>} The transaction's result.
+   */
+  async sendTransaction(tx) {
+    throw new NotImplementedError('sendTransaction(tx)')
+  }
+
+  /**
+   * Quotes the costs of a send transaction operation.
+   *
+   * @see {sendTransaction}
+   * @param {Transaction} tx - The transaction.
+   * @returns {Promise<Omit<TransactionResult, 'hash'>>} The transaction's quotes.
+   */
+  async quoteSendTransaction(tx) {
+    throw new NotImplementedError('quoteSendTransaction(tx)')
+  }
+
+  /**
+   * Transfers a token to another address.
+   *
+   * @param {TransferOptions} options - The transfer's options.
+   * @returns {Promise<TransferResult>} The transfer's result.
+   */
+  async transfer(options) {
+    throw new NotImplementedError('transfer(options)')
+  }
+
+  /**
+   * Quotes the costs of a transfer operation.
+   *
+   * @see {transfer}
+   * @param {TransferOptions} options - The transfer's options.
+   * @returns {Promise<Omit<TransferResult, 'hash'>>} The transfer's quotes.
+   */
+  async quoteTransfer(options) {
+    throw new NotImplementedError('quoteTransfer(options)')
+  }
+
+  /**
+   * Disposes the wallet account, erasing the private key from the memory.
+   */
+  dispose() {
+    throw new NotImplementedError('dispose()')
+  }
+}
