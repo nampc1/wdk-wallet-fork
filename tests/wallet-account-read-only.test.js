f5208dc04b2677aa79b2e1e72d0c0f2c33337552
Davide Casale <davi0kprogramsthings@gmail.com>
2025-07-30 02:38:01 +0200

    Fix unit test.

--- tests/wallet-account-read-only.test.js
+++ tests/wallet-account-read-only.test.js
@@ -1,6 +1,4 @@
-import * as bip39 from 'bip39'
-
-import { describe, expect, test } from '@jest/globals'
+import { beforeEach, describe, expect, test } from '@jest/globals'
 
 import { AbstractWalletAccountReadOnly } from '../index.js'
 
@@ -29,11 +27,17 @@ class DummyWalletAccountReadOnly extends AbstractWalletAccountReadOnly {
 const ADDRESS = '0xa460AEbce0d3A4BecAd8ccf9D6D4861296c503Bd'
 
 describe('AbstractWalletAccountReadOnly', () => {
-  describe('constructor', () => {
-    test('should successfully initialize a read-only wallet account for the given address', async () => {
-      const account = new DummyWalletAccountReadOnly(ADDRESS)
+  let account
 
-      expect(await account.getAddress()).toEqual(ADDRESS)
+  beforeEach(() => {
+    account = new DummyWalletAccountReadOnly(ADDRESS)
+  })
+
+  describe('getAddress', () => {
+    test('should return the correct address', async () => {
+      const address = await account.getAddress()
+
+      expect(address).toBe(ADDRESS)
     })
   })
 })
